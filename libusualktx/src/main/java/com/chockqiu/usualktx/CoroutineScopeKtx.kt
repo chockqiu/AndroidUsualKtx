f24c88d3e34be80311ed1ae926304b1aa0c6ad48
Q <Q@98du.com>
2023-07-24 15:31:26 +0800

    feat: 新增CoroutineScopeKtx扩展函数

--- libusualktx/src/main/java/com/chockqiu/usualktx/CoroutineScopeKtx.kt
+++ libusualktx/src/main/java/com/chockqiu/usualktx/CoroutineScopeKtx.kt
@@ -1,8 +1,6 @@
 package com.chockqiu.usualktx
 
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.CoroutineStart
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.*
 import kotlin.coroutines.CoroutineContext
 
 object CoroutineScopeKtx {
@@ -27,6 +25,73 @@ object CoroutineScopeKtx {
         } catch (e: Throwable) {
             e.printStackTrace()
             fail?.invoke(this, e)
+        }
+    }
+
+
+    fun CoroutineScope.launchIOCatch(
+        context: CoroutineContext = Dispatchers.IO,
+        start: CoroutineStart = CoroutineStart.DEFAULT,
+        fail: (suspend CoroutineScope.(e: Throwable) -> Unit)? = null,
+        block: suspend CoroutineScope.() -> Unit
+    ) = launch(context, start) {
+        try {
+            this.block()
+        } catch (e: Throwable) {
+            e.printStackTrace()
+            fail?.invoke(this, e)
+        }
+    }
+
+    fun CoroutineScope.launchDefaultCatch(
+        context: CoroutineContext = Dispatchers.Default,
+        start: CoroutineStart = CoroutineStart.DEFAULT,
+        fail: (suspend CoroutineScope.(e: Throwable) -> Unit)? = null,
+        block: suspend CoroutineScope.() -> Unit
+    ) = launch(context, start) {
+        try {
+            this.block()
+        } catch (e: Throwable) {
+            e.printStackTrace()
+            fail?.invoke(this, e)
+        }
+    }
+
+    fun CoroutineScope.launchMainCatch(
+        context: CoroutineContext = Dispatchers.Main,
+        start: CoroutineStart = CoroutineStart.DEFAULT,
+        fail: (suspend CoroutineScope.(e: Throwable) -> Unit)? = null,
+        block: suspend CoroutineScope.() -> Unit
+    ) = launch(context, start) {
+        try {
+            this.block()
+        } catch (e: Throwable) {
+            e.printStackTrace()
+            fail?.invoke(this, e)
+        }
+    }
+
+    suspend fun withContextMain(
+        block: suspend CoroutineScope.() -> Unit
+    ) {
+        withContext(Dispatchers.Main) {
+            block()
+        }
+    }
+
+    suspend fun withContextDefault(
+        block: suspend CoroutineScope.() -> Unit
+    ) {
+        withContext(Dispatchers.Default) {
+            block()
+        }
+    }
+
+    suspend fun withContextIO(
+        block: suspend CoroutineScope.() -> Unit
+    ) {
+        withContext(Dispatchers.IO) {
+            block()
         }
     }
 }
